806cc89106988556bc97d818b408d45bfb55e8be
perfer95 <perfer95@outlook.com>
2024-05-11 09:20:40 +0000

    9-print_last_digit.py

---
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+#9-print_last_digit.py
+
+def print_last_digit(number):
+    """
+    Function that prints the last digit of a number.
+    """
+    lastnum = abs(number) % 10
+
+    print(f"{lastnum}")
+
+    return lastnum
