da22a31ceefd1f3fefca636045c09ebcf00e378a
perfer95 <perfer95@outlook.com>
2024-05-11 22:28:26 +0000

    1-calculation.py

---
+++ python-import_modules/1-calculation.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+# 1-calculation.py
+
+if __name__ == "__main__":
+    from 1-calculation import add, sub, mul, div
+
+    a = 10
+    b = 5
+    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+    print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
+    print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
+    print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
