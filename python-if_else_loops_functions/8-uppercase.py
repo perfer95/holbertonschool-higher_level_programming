cab1d13514ff378bfd248cc2466b489d5640229a
perfer95 <perfer95@outlook.com>
2024-05-11 09:05:33 +0000

    8-uppercase.py

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -7,5 +7,6 @@ def uppercase(str):
     """
     for i in str:
         if ord(i) >= 97 and ord(i) <= 122:
-            print("{}".format(chr(ord(i) - 32)), end="")
+            i = chr(ord(c) - 32)
+        print("{}".format(i), end="")
     print("")
