6b6e0bc5fec6a348aa5a37df741de2ee3f0a0fda
perfer95 <perfer95@outlook.com>
2024-05-11 09:59:30 +0000

    12-fizzbuzz.py

---
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+# 12-fizzbuzz.py
+
+def fizzbuzz():
+    """
+    Function that prints the numbers from 1 to 100 separated by a space
+    """
+    for i in range(1, 101):
+        if i % 3 == 0 and i % 5 == 0:
+            print("FizzBuzz", end="")
+        elif i % 3 == 0:
+            print("Fizz", end="")
+        elif i % 5 == 0:
+            print("Buzz", end="")
+        else:
+            print(f"{i}", end="")
