06f93bed83fde8f878fdcc4593170dc4c322ccf0
perfer95 <perfer95@outlook.com>
2024-05-11 22:51:46 +0000

    2-args.py

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+# 2-args.py
+
+if __name__ == "__main__":
+
+    arg_v = sys.argv
+    len_argv = len(arg_v) - 1 #Just arguments no file name
+
+    if len_argv >= 1:
+        print(arg_v, "arguments:")
+        for i in range(1, len_argv + 1):
+            print("{:d}: {}".format(i, arg_v[i]))
+    elif len_argv == 0:
+        print(len_argv, "arguments.")
+
