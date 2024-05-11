11cd64ec77ef1cc673071ebba8ad83b7a2d228c9
perfer95 <perfer95@outlook.com>
2024-05-11 08:39:13 +0000

    7-islower.py

---
+++ python-if_else_loops_functions/7-islower.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+def islower(c):
+    """
+    Function that verify if a char variable(alpha) is lower
+    Return True if c is lowercase
+    Return False if c is not lowercase
+    """
+    if ord(c) >= 97 and ord(c) <= 122:
+        return True
+    else
+        return False
