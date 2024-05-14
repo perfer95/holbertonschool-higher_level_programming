81bcc5fed05b4fbe2ac47df8c35a6755b9b8293c
perfer95 <perfer95@outlook.com>
2024-05-14 04:09:51 +0000

    6-print_matrix_integer.py

---
+++ python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+# 6-print_matrix_integer.py
+
+def print_matrix_integer(matrix=[[]]):
+    """
+    Function that prints a matrix of integers.
+    """
+
+    for i in range(0, len(matrix)):
+        for j in range(0, len(matrix[i])):
+            if j != (len(matrix[i]) - 1):
+                print("{:d} ".format(matrix[i][j]), end="")
+            else:
+                print("{:d}".format(matrix[i][j]))
