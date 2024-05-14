34f1b8810232e91b27ac30d84fe353fe737c7007
perfer95 <perfer95@outlook.com>
2024-05-14 02:08:37 +0000

    2-replace_in_list.py

--- python-data_structures/2-replace_in_list.py
+++ python-data_structures/2-replace_in_list.py
@@ -8,7 +8,7 @@ def replace_in_list(my_list, idx, element):
     len_list = len(my_list)
 
     if idx < 0 or idx > (len_list - 1):
-        return None
+        return my_list
     else:
         my_list[idx] = element
         return my_list
