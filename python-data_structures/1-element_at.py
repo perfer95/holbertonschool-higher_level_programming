d4455aa7d78b3ce61adfd2cbbed418551b94bc1c
perfer95 <perfer95@outlook.com>
2024-05-14 01:08:23 +0000

    1-element_at.py

---
+++ python-data_structures/1-element_at.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+# 1-element_at.py
+
+def element_at(my_list, idx):
+    """
+    Function that retrieves an element from a list.
+    """
+    len_list = len(idx)
+
+    if idx  < 0 or idx > (len_list - 1):
+        return None
+    else:
+        return my_list[idx]
