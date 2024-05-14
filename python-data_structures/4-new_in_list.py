58530c1c92d5fd62fc6289a157bb5445ae913623
perfer95 <perfer95@outlook.com>
2024-05-14 03:27:43 +0000

    4-new_in_list.py

---
+++ python-data_structures/4-new_in_list.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+# 4-new_in_list.py
+
+def new_in_list(my_list, idx, element):
+    """
+     function that replaces an element in a list at a specific
+     position without modifying the original list
+    """
+    new_list = my_list
+    len_list = len(my_list)
+
+    if idx < 0 or idx > (len_list - 1):
+        return new_list
+    else:
+        new_list[idx] = element
+        return new_list
