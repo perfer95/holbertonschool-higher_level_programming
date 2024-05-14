9802c6ff53035f847d0ed953a29848c37e3845cc
perfer95 <perfer95@outlook.com>
2024-05-14 02:05:06 +0000

    2-replace_in_list.py

---
+++ python-data_structures/2-replace_in_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+# 2-replace_in_list.py
+
+def replace_in_list(my_list, idx, element):
+    """
+    Function that replaces an element of a list at a specific position.
+    """
+    len_list = len(my_list)
+
+    if idx < 0 or idx > (len_list - 1):
+        return None
+    else:
+        my_list[idx] = element
+        return my_list
