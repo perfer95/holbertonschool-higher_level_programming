dbc681d6685ea072b7a5dcdf64fde19cbafaae9e
perfer95 <perfer95@outlook.com>
2024-05-15 09:07:31 +0000

    9-max_integer.py

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -9,12 +9,11 @@ def max_integer(my_list=[]):
 
     if my_list == []:
         return None
-    
+
     val_max = my_list[0]
 
     for i in range(1, len(my_list)):
         if val_max < my_list[i]:
             val_max = my_list[i]
 
-    return val_max
-        
+    return val_max        
