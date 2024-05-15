e25060b4afe59e62866121642e8dfa3c13dddc5a
perfer95 <perfer95@outlook.com>
2024-05-15 09:21:00 +0000

    10-divisible_by_2.py

---
+++ python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+# 10-divisible_by_2.py
+
+def divisible_by_2(my_list=[]):
+    """
+    Function that finds all multiples of 2 in a list.
+    """
+    new_list = []
+    
+    if my_list == []:
+        return None
+
+    for i in range(0, len(my_list)):
+        if my_list[i] % 2 == 0:
+            new_list.append(True)
+        else:
+            new_list.append(False)
+
+    return new_list
