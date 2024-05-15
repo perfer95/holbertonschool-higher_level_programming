9999e3161b0b6026b39c47c598cd5554be9fbd81
perfer95 <perfer95@outlook.com>
2024-05-15 08:45:06 +0000

    7-add_tuple.py

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -20,4 +20,6 @@ def add_tuple(tuple_a=(), tuple_b=()):
         else:
             tuple_b = (tuple_b[0], 0)
 
-    return (new_tuple = (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1]))
+    new_tuple = (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
+
+    return (new_tuple)
