7d8a93348f32773f5f2df618bffebeab5da21af3
perfer95 <perfer95@outlook.com>
2024-05-17 20:15:38 +0000

    6-print_sorted_dictionary.py

---
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+# 6-print_sorted_dictionary.py
+
+def print_sorted_dictionary(a_dictionary):
+    """
+    Function that prints a dictionary by ordered keys.
+    """
+    tupla_sort = set(a_dictionary.keys())
+
+    for i in range(tupla_sort):
+        print("{}: {}".format(i, a_dictionary[i]))
+
