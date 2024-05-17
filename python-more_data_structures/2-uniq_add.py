6b0110da6974519ec23c77997e9ec8c700cb5859
perfer95 <perfer95@outlook.com>
2024-05-17 08:18:33 +0000

    2-uniq_add.py

---
+++ python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+# 2-uniq_add.py
+
+def uniq_add(my_list=[]):
+    """
+    function that adds all unique integers in a
+    list (only once for each integer).
+    """
+    set_list = set(my_list)
+    sum_list = 0
+
+    for i in set_list:
+        sum_list += set_list
+
+    return sum_list
