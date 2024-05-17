d6706d4c41c809c0f86820415c7d871fc5a9e540
perfer95 <perfer95@outlook.com>
2024-05-17 20:49:08 +0000

    7-update_dictionary.py

---
+++ python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+# 7-update_dictionary.py
+
+def update_dictionary(a_dictionary, key, value):
+    """
+    Function that replaces or adds key/value in a dictionary.
+    """
+    a_dictionary[key] = value
