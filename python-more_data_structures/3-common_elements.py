a07bb210d8cf6b2d76a4912057ae5628f5d355ad
perfer95 <perfer95@outlook.com>
2024-05-17 08:23:18 +0000

    3-common_elements.py

---
+++ python-more_data_structures/3-common_elements.py
@@ -0,0 +1,8 @@
+#1/usr/bin/python3
+# 3-common_elements.py
+
+def common_elements(set_1, set_2):
+    """
+    Function that returns a set of common elements in two sets.
+    """
+    return(set_1 | set_2)
