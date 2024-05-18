f6ab1fa803b39abe571331175f5e5da36c9589d4
perfer95 <perfer95@outlook.com>
2024-05-18 22:27:47 +0000

    0-safe_print_list.py

---
+++ python-exceptions/0-safe_print_list.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+# 0-safe_print_list.py
+
+def safe_print_list(my_list=[], x=0):
+    """
+    Function that prints x elements of a list.
+    """
+    count = 0
+
+    for i in range(0, x):
+        try:
+            print("{}".format(my_list[i]), end="")
+            count += 1
+        except IndexError:
+            break
+
+    print("")
+    return count
