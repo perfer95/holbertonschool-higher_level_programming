f64d8397291750546548d1115a36e9ff59bf32f8
perfer95 <perfer95@outlook.com>
2024-05-18 04:07:17 +0000

    12-roman_to_int.py

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -5,9 +5,17 @@ def roman_to_int(roman_string):
     """
     Function that converts a Roman numeral to an integer.
     """
-    if not isinstance(roman_string, str) or roman_string == None:
+    if (not isinstance(roman_string, str)) or (roman_string == None):
         return 0
-    roman_nums = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+    roman_nums = {
+            "I": 1,
+            "V": 5,
+            "X": 10,
+            "L": 50,
+            "C": 100,
+            "D": 500,
+            "M": 1000
+    }
     result = 0
     num = ""
     roman_string = roman_string[::-1]
