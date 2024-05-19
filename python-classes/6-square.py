319f83c4b1f44937ff12b29bc88078eb8b7c070e
perfer95 <perfer95@outlook.com>
2024-05-19 04:11:16 +0000

    6-square.py

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -59,8 +59,11 @@ class Square:
         if self.__size == 0:
             print("")
         else:
-            [print("") for i in range(0, self.__position[1])]
+            for i in range(0, self.__position[1]):
+                print("")
             for i in range(0, self.__size):
-                [print(" ", end="") for j in range(0, self.__position[0])]
-                [print("#", end="") for j in range(0, self.__size)]
+                for j in range(0, self.__position[0]):
+                    print(" ", end="")
+                for k in range(0, self.__size):
+                    print("#", end="")
                 print("")
