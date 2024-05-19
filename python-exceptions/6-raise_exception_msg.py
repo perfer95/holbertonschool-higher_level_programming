e8fd1262d15a5311a26510ab7c2b814f20a0ce0e
perfer95 <perfer95@outlook.com>
2024-05-19 01:50:35 +0000

    6-raise_exception_msg.py

---
+++ python-exceptions/6-raise_exception_msg.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+# 6-raise_exception_msg.py
+
+def raise_exception_msg(message=""):
+    """
+    function that raises a name exception with a message.
+    """
+    raise NameError(message)
