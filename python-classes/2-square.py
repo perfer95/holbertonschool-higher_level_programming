6055d6c10e66e638616bdb7a2e95f73a92e8b478
perfer95 <perfer95@outlook.com>
2024-05-19 02:57:35 +0000

    2-square.py

---
+++ python-classes/2-square.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+# 2-square.py
+"""
+Define class Square
+"""
+
+
+class Square:
+    """
+    Defines a square:
+    """
+
+    def __init__(self, size):
+        """
+        New Square
+
+        Args:
+            size: size of square (int)
+        """
+
+        if type(size) is int:
+            if size < 0:
+                rise ValueError('size must be >= 0')
+            else:
+                self.__size = size
+        else:
+            raise TypeError('size must be an integer')
