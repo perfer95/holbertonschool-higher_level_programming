241b6631ef87328b7b770cd834119c7364bb2463
perfer95 <perfer95@outlook.com>
2024-05-19 01:37:54 +0000

    4-list_division.py

---
+++ python-exceptions/4-list_division.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+# 4-list_division.py
+
+def list_division(my_list_1, my_list_2, list_length):
+    """
+    Function that divides element by element 2 lists.
+    """
+    new_list = []
+    result_div = 0
+
+    for i in range(0, list_length):
+        try:
+            result_div = my_list_1[i] / my_list_2[i]
+        except ZeroDivisionError:
+            print("wrong type")
+            result_div = 0
+        except ZeroDivisionError:
+            print("division by 0")
+            result_div = 0
+        except IndexError:
+            print("out of range")
+            result_div = 0
+        finally:
+            new_list.append(result_div)
+
+    return (new_list)
