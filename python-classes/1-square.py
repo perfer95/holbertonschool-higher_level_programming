0970fcd8302a76cb21fbb685063fdd43f18e4a71
perfer95 <perfer95@outlook.com>
2024-05-19 02:47:12 +0000

    1-square.py

---
+++ python-classes/1-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+# 1-square.py
+"""
+Define class Square
+"""
+
+
+class Square:
+    """
+    Defines a square:
+    """
+
+    def __init__(self, size):
+        """
+        New Square
+
+        Args:
+            size: size of square (int)
+        """
+
+        self.__size = size
