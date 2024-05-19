02abeb337dc9f68c0598d4673937195e7c16e09d
perfer95 <perfer95@outlook.com>
2024-05-19 03:16:19 +0000

    4-square.py

---
+++ python-classes/4-square.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+# 4-square.py
+"""
+Define class Square
+"""
+
+
+class Square:
+    """
+    Defines a square:
+    """
+
+    def __init__(self, size=0):
+        """
+        New Square
+
+        Args:
+            size: size of square (int)
+        """
+        self.size = size
+
+   @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, size):
+        if type(size) is int:
+            if size < 0:
+                raise ValueError('size must be >= 0')
+            else:
+                self.__size = size
+        else:
+            raise TypeError('size must be an integer')
+
+    def area(self):
+        """
+        Return the area of the square
+        """
+        return self.__size * self.__size
