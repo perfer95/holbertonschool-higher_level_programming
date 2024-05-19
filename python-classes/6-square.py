cf2d0a897f05cb2fcf063319d4aa5945c3ff672c
perfer95 <perfer95@outlook.com>
2024-05-19 04:04:55 +0000

    6-square.py

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -66,4 +66,4 @@ class Square:
                     print(" ", end="")
                 for k in range(0, self.__size):
                     print("#", end="")
-            print("")
+                print("")
