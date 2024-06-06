36e0d0e794b14aeb8054b473c28696c9299ed21d
perfer95 <perfer95@outlook.com>
2024-06-06 23:06:04 +0000

    0. Integers addition

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -5,6 +5,9 @@ Here is de excersice of add two integers
 """
 
 def add_integer(a, b=98):
+    """
+    Function which return the value of add two integers
+    """
     if not(isinstance(a, int) or isinstance(a, float)):
         raise TypeError("a must be an integer")
     if not(isinstance(b, int) or isinstance(b, float)):
