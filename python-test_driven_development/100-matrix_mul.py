8451699eb2889c683865af5bc70cdc0f7e5f5032
perfer95 <perfer95@outlook.com>
2024-06-30 23:23:04 +0000

    100-matrix_mul

---
+++ python-test_driven_development/100-matrix_mul.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+6. Matrix multiplication
+"""
+
+
+def matrix_mul(m_a, m_b):
+    """
+    Function that multiplies 2 matrices
+    Return: a matrice of result of multiplying two matrices
+    """
+    if not isinstance(m_a, list):
+        raise TypeError("m_a must be a list")
+    if not isinstance(m_b, list):
+        raise TypeError("m_b must be a list")
+
+    if not all(isinstance(row, list) for row in m_a):
+        raise TypeError("m_a must be a list of lists")
+    if not all(isinstance(row, list) for row in m_b):
+        raise TypeError("m_b must be a list of lists")
+
+    if m_a == [] or m_a == [[]]:
+        raise ValueError("m_a can't be empty")
+    if m_b == [] or m_b == [[]]:
+        raise ValueError("m_b can't be empty")
