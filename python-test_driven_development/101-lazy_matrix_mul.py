766ea8b0c85b7297beddcc16dc667e1a7e0ca935
perfer95 <perfer95@outlook.com>
2024-07-02 05:01:33 +0000

    101-lazy_matrix_mul

--- python-test_driven_development/101-lazy_matrix_mul.py
+++ python-test_driven_development/101-lazy_matrix_mul.py
@@ -21,9 +21,11 @@ def lazy_matrix_mul(m_a, m_b):
         raise TypeError("m_b must be a list of lists")
 
     if m_a == [] or m_a == [[]]:
-        raise ValueError("shapes (1,0) and (2,2) not aligned: 0 (dim 1) != 2 (dim 0)")
+        raise ValueError("shapes (1,0) and (2,2) not aligned: 0 (dim 1) "
+                         "!= 2 (dim 0)")
     if m_b == [] or m_b == [[]]:
-        raise ValueError("shapes (2,2) and (1,0) not aligned: 2 (dim 1) != 1 (dim 0)")
+        raise ValueError("shapes (2,2) and (1,0) not aligned: 2 (dim 1) "
+                         "!= 1 (dim 0)")
 
     for row in m_a:
         for elem in row:
@@ -40,6 +42,7 @@ def lazy_matrix_mul(m_a, m_b):
         raise TypeError("setting an array element with a sequence.")
 
     if len(m_a[0]) != len(m_b):
-        raise ValueError("shapes (2,3) and (2,2) not aligned: 3 (dim 1) != 2 (dim 0)")
+        raise ValueError("shapes (2,3) and (2,2) not aligned: 3 (dim 1) "
+                         "!= 2 (dim 0)")
 
     return np.matmul(m_a, m_b)
