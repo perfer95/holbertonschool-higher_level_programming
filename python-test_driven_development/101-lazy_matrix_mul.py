e51effcb1d663d43ac6c8d46815875056b4a90c4
perfer95 <perfer95@outlook.com>
2024-07-02 03:47:35 +0000

    101-lazy_matrix_mul

--- python-test_driven_development/101-lazy_matrix_mul.py
+++ python-test_driven_development/101-lazy_matrix_mul.py
@@ -5,8 +5,9 @@
 """
 import numpy as np
 
+
 def lazy_matrix_mul(m_a, m_b):
-"""
-Function that multiplies 2 matrices by using the module NumPy
-"""
-return (np.matmul(m_a, m_b))
+    """
+    Function that multiplies 2 matrices by using the module NumPy
+    """
+    return (np.matmul(m_a, m_b))
