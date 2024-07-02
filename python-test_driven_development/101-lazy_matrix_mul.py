18b8d0950a9abe19a8f9f7cfc60dc5c4521aebd8
perfer95 <perfer95@outlook.com>
2024-07-02 00:49:41 +0000

    100-matrix_mul

---
+++ python-test_driven_development/101-lazy_matrix_mul.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+"""
+7. Lazy matrix multiplication
+"""
+
+
+def lazy_matrix_mul(m_a, m_b):
+"""
+Function that multiplies 2 matrices by using the module NumPy
+"""
+return (np.matmul(m_a, m_b))
