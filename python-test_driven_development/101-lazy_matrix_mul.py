1925ceaa54006d7b47ddf9cc95115bbd16c6dc49
perfer95 <perfer95@outlook.com>
2024-07-02 04:36:06 +0000

    101-lazy_matrix_mul

--- python-test_driven_development/101-lazy_matrix_mul.py
+++ python-test_driven_development/101-lazy_matrix_mul.py
@@ -23,7 +23,7 @@ def lazy_matrix_mul(m_a, m_b):
     if m_a == [] or m_a == [[]]:
         raise ValueError("shapes (1,0) and (2,2) not aligned: 0 (dim 1) != 2 (dim 0)")
     if m_b == [] or m_b == [[]]:
-        raise ValueError("shapes (1,0) and (2,2) not aligned: 0 (dim 1) != 2 (dim 0)")
+        raise ValueError("shapes (2,2) and (1,0) not aligned: 2 (dim 1) != 1 (dim 0)")
 
     for row in m_a:
         for elem in row:
