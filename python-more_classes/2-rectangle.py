c907d0145b1dc5827b0d9235f710ed594a0d57ae
perfer95 <perfer95@outlook.com>
2024-07-06 02:31:59 +0000

    2-rectangle.py

---
+++ python-more_classes/2-rectangle.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python3
+"""
+2. Area and Perimeter
+"""
+
+
+class Rectangle:
+    """
+    Class that defines a rectangle
+    """
+    def __init__(self, width=0, height=0):
+        """ Initialize rectangugle attributes """
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """ width getter """
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """ width setter """
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+
+        self.__width = value
+
+    @property
+    def height(self):
+        """ Height getter """
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """ Height setter """
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+
+        self.__height = value
+
+    def area(self):
+        """ Calculate area of the rectangle """
+        return self.__width * self.__height
+
+    def perimeter(self):
+        """ Calcilate perimeter of the rectangle """
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        
+        return (self.__width * 2) + (self.__height * 2)
