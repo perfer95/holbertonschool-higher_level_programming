5473ffeaa8a686f3688da83c7339225985be3f19
perfer95 <perfer95@outlook.com>
2024-07-08 20:37:46 +0000

    7. Change representation

--- python-more_classes/7-rectangle.py
+++ python-more_classes/7-rectangle.py
@@ -65,7 +65,7 @@ class Rectangle:
         rectangle = []
 
         for i in range(self.__height):
-            [rectangle.append(self.print_symbol) for j in range(self.__width)]
+            [rectangle.append(str(self.print_symbol)) for j in range(self.__width)]
             if i != self.__height - 1:
                 rectangle.append("\n")
         return ("".join(rectangle))
