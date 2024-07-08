44c5515607590cfecfa7c605cc7680416a9c1116
perfer95 <perfer95@outlook.com>
2024-07-08 20:35:32 +0000

    7. Change representation

---
+++ python-more_classes/7-rectangle.py
@@ -0,0 +1,80 @@
+#!/usr/bin/python3
+"""
+7. Change representation
+"""
+
+
+class Rectangle:
+    """
+    Class that defines a rectangle
+    """
+    number_of_instances = 0
+    print_symbol = "#"
+
+    def __init__(self, width=0, height=0):
+        """ Initialize rectangugle attributes """
+        Rectangle.number_of_instances += 1
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """ width getter """
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """ width setter """
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+
+        self.__width = value
+
+    @property
+    def height(self):
+        """ Height getter """
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """ Height setter """
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+
+        self.__height = value
+
+    def area(self):
+        """ Calculate area of the rectangle """
+        return self.__width * self.__height
+
+    def perimeter(self):
+        """ Calculate perimeter of the rectangle """
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        return (self.__width * 2) + (self.__height * 2)
+
+    def __str__(self):
+        """ Rectangle informal representation """
+        if self.__width == 0 or self.__height == 0:
+            return ""
+
+        rectangle = []
+
+        for i in range(self.__height):
+            [rectangle.append(self.print_symbol) for j in range(self.__width)]
+            if i != self.__height - 1:
+                rectangle.append("\n")
+        return ("".join(rectangle))
+
+    def __repr__(self):
+        """ Rectangle formal representation """
+        return f"Rectangle({self.width}, {self.height})"
+
+    def __del__(self):
+        """ Message delete """
+        Rectangle.number_of_instances -= 1
+        print("Bye rectangle...")
