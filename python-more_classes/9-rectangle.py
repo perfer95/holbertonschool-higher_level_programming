3e8860a1badfb0ea630adb5a7b44732a1078bc69
perfer95 <perfer95@outlook.com>
2024-07-08 21:03:24 +0000

    9. A square is a rectangle

--- python-more_classes/9-rectangle.py
+++ python-more_classes/9-rectangle.py
@@ -90,11 +90,11 @@ class Rectangle:
         if rect_1.area() >= rect_2.area():
             return rect_1
         return rect_2
-    
+
     @classmethod
     def square(cls, size=0):
         """
-        Class method that returns a new Rectangle instance 
+        Class method that returns a new Rectangle instance
         with width == height == size
         """
         return cls(size, size)
