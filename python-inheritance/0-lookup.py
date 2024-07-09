bf2856aa0b3d0acdf3fb3a9b7d22768bbbcc7437
perfer95 <perfer95@outlook.com>
2024-07-09 19:19:40 +0000

    0. Lookup

---
+++ python-inheritance/0-lookup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+0. Lookup
+"""
+
+
+def lookup(obj):
+    """
+    Function that return the list of available attributes and methods
+    of an object
+    """
+    return dir(obj)
