bd3632ca9ab998b7f96dbabecea21d4dc6270396
perfer95 <perfer95@outlook.com>
2024-07-10 20:51:10 +0000

    9. Full rectangle

---
+++ python-inheritance/9-rectangle.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+8. Rectangle
+"""
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """ BaseGeometry Class """
+    def __init__(self, width, height):
+        """ Cosntruct """
+        self.integer_validator("width", width)
+        self.__width = width
+        self.integer_validator("height", height)
+        self.__height = height
+
+    def area(self):
+        """ Area of rectangle """
+        return self.__width * self.__height
+
+    def __str__(self):
+        """ Rectangle description """
+        return f"[Rectangle] {self.__width}/{self.__height}"
