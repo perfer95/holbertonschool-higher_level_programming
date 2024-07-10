900a243debc02670a19eb064fb90905e8bade526
perfer95 <perfer95@outlook.com>
2024-07-10 00:50:00 +0000

    3. Same class or inherit from

--- python-inheritance/3-is_kind_of_class.py
+++ python-inheritance/3-is_kind_of_class.py
@@ -6,7 +6,8 @@
 
 def is_kind_of_class(obj, a_class):
     """
-    Returns True if the object is an instance of, or if the object is an instance
-    of a class that inherited from, the specified class ; otherwise False
+    Returns True if the object is an instance of, or if the object is an
+    instance of a class that inherited from, the specified class; 
+    otherwise False
     """
     return isinstance(obj, a_class)
