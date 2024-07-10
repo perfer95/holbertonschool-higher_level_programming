b6ab757ddaada1b430238d8f32ee4849614d53ad
perfer95 <perfer95@outlook.com>
2024-07-10 20:56:37 +0000

    9. Full rectangle

---
+++ python-inheritance/10-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+10. Square #1
+"""
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """ Square Representation """
+    def __init__(self, size):
+        """ Cosntructor """
+        self.integer_validator("size", size)
+        self.__size = size
+
+    def area(self):
+        """ Square Area"""
+        return self.__size * self.__size
