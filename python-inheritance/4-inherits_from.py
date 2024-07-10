9db5ce306b6cd2a7e142308b2276ef40d52cd8b9
perfer95 <perfer95@outlook.com>
2024-07-10 07:00:45 +0000

    4. Only sub class of

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -10,4 +10,4 @@ def inherits_from(obj, a_class):
     inherited (directly or indirectly) from the specified class;
     otherwise False.
     """
-    return (issubclass(type(obj), a_class) and type(obj) is a_class)
+    return (issubclass(type(obj), a_class) and (type(obj) is not a_class))
