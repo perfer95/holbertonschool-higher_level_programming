541ed5d169383cfd048ed15e87ce3c44cf13e5f8
perfer95 <perfer95@outlook.com>
2024-07-11 20:13:15 +0000

    0. Abstract Animal Class and its Subclasses

---
+++ python-abc/task_00_abc.py
@@ -0,0 +1,23 @@
+#/usr/bin/python3
+"""
+0. Abstract Animal Class and its Subclasses
+"""
+from abc import ABC, abstractmethod
+
+
+class Animal(ABC):
+    """ Abstrac Class """
+     @abstractmethod
+     def sound(self):
+         """ Sound Abstrac Method"""
+         pass
+
+class Dog(Animal):
+    """ Dog class """
+    def sound(self):
+        return "Bark"
+
+class Cat(Animal):
+    """ Cat class """
+    def sound(self):
+        return "Meow"
