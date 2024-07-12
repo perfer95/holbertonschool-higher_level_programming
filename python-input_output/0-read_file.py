9b7200eb2e03c502b6209c71d0dae1f13854f839
perfer95 <perfer95@outlook.com>
2024-07-12 23:35:07 +0000

    0. Read file

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -8,4 +8,3 @@ def read_file(filename=""):
     """ Reads a text file (UTF8) and prints it to stdout """
     with open(filename, "r", encoding="UTF-8") as f:
         print(f"{f.read()}", end="")
-
