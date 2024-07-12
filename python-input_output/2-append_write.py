bbebb80c75112a6921083906c77d36c1b73f6cc0
perfer95 <perfer95@outlook.com>
2024-07-12 23:55:13 +0000

    2. Append to a file

---
+++ python-input_output/2-append_write.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+2. Append to a file
+"""
+
+
+def append_write(filename="", text=""):
+    """
+    Appends a string at the end of a text file (UTF8) and
+    returns the number of characters added
+    """
+    with open(filename, "a", encoding="UTF-8") as file:
+        len_add_char = filename.write(text)
+
+    return len_add_char
