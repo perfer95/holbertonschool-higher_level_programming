29754d43c11488360936cbc9d28b8cc42740d97b
perfer95 <perfer95@outlook.com>
2024-07-17 01:23:48 +0000

    8. Class to JSON

---
+++ python-input_output/8-class_to_json.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+8. Class to JSON
+"""
+
+
+def class_to_json(obj):
+    """ 
+    Returns the dictionary description with simple data structure
+    (list, dictionary, string, integer and boolean) for JSON 
+    serialization of an object
+    """
+    return obj.__dict__
