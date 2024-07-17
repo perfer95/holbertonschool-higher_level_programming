d09d83a39fe904b207e2d0dcd7df06f548b3d335
perfer95 <perfer95@outlook.com>
2024-07-17 01:55:23 +0000

    11. Student to disk and reload

---
+++ python-input_output/11-student.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+11. Student to disk and reload
+"""
+
+
+class Student:
+    """ Student Class """
+    def __init__(self, first_name, last_name, age):
+        """ Student Constructor """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """ Retrieves a dictionary representation of a Student instance """
+        if isinstance(attrs, list) and \
+                all(isinstance(ele, str) for ele in attrs):
+            """ dictionary comprehension """
+            return {i: getattr(self, i) for i in attrs if hasattr(self, i)}
+        return self.__dict__
+
+    def reload_from_json(self, json):
+        """ Replaces all attributes of the Student instance """
+        for k, v in json.items():
+            setattr(self, k, v)
