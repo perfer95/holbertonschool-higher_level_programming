4ba7623bdb1a6470be9724f9f1954874e0ba880e
perfer95 <perfer95@outlook.com>
2024-07-20 04:56:26 +0000

    2. Consuming and processing data from an API using Python

--- restful-api/task_02_requests.py
+++ restful-api/task_02_requests.py
@@ -17,21 +17,16 @@ def fetch_and_print_posts():
 
         for data in f_json:
             print(data['title'])
-    else:
-        pass
-
 
 def fetch_and_save_posts():
     """ fetches all post from JSONPlaceholder an save data """
     res = requests.get('https://jsonplaceholder.typicode.com/posts')
 
     if res.status_code == 200:
-        data = response.json()
+        data = res.json()
         data_to_write = [{'id': post['id'], 'title': post['title'], 'body': post['body']} for post in data]
 
         with open('posts.csv', 'w', newline='') as file:
             writer = csv.DictWriter(file, fieldnames=['id', 'title', 'body'])
             writer.writeheader()
             writer.writerows(data_to_write)
-    else:
-        pass
